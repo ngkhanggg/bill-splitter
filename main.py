ab40547c392d94d5d3b1cc6ccb3141f934259b20
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-10-11 04:12:15 +0700

    Finish up the remainding logic

--- main.py
+++ main.py
@@ -79,7 +79,10 @@ def main(cost_per_hour, file_name):
     print(f"log\n{log}")
     
     new_log = transform_log(log)
-    print(f"new_log {new_log}")
+    print(f"new_log\n{new_log}")
+    
+    list_players = {key: 0.0 for key in log.keys()}
+    print(f"list_players\n{list_players}")
     
     sorted_start_times, sorted_end_times = sort_times(new_log)
     print(f"sorted_start_times\n{sorted_start_times}")
@@ -94,6 +97,36 @@ def main(cost_per_hour, file_name):
     
     combinations = create_combinations(merged_list)
     print(f"combinations\n{combinations}")
+    
+    divisions = []
+    for combination in combinations:
+        tmp = []
+        for k, v in new_log.items():
+            if is_within_interval(combination, v):
+                tmp.append(k)
+        divisions.append({'interval': combination, 'player(s)': tmp})
+    print(f"divisions\n{divisions}")
+    
+    new_divisions = []
+    for division in divisions:
+        elapsed_time = calculate_elapsed_time(division['interval'][0], division['interval'][1])
+        new_divisions.append({elapsed_time: division['player(s)']})
+    print(f"new_divisions\n{new_divisions}")
+    
+    for player, fees in list_players.items():
+        print(f"player {player} fees {fees}")
+        for division in new_divisions:
+            print(f"division {division}")
+            for k, v in division.items():
+                print(f"k {k} v {v}")
+                if player in v:
+                    list_players[player] += (k / len(v))
+    print(list_players)
+    
+    for player, fees in list_players.items():
+        list_players[player] = fees * cost_per_hour
+    
+    print(list_players)
 
 
 if __name__ == '__main__':
