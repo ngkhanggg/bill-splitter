94b03124704030fffc2fee5e747397401b77869b
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-10-11 04:26:31 +0700

    Refactor and remove print statements so that I will suffer debugging in the future

--- main.py
+++ main.py
@@ -63,55 +63,30 @@ def calculate_elapsed_time(time1, time2):
 
 def main(cost_per_hour, file_name):
     log = get_log(file_name)
-    print(f"log\n{log}")
-    
     new_log = transform_log(log)
-    print(f"new_log\n{new_log}")
-    
-    list_players = {key: 0.0 for key in log.keys()}
-    print(f"list_players\n{list_players}")
     
     sorted_start_times, sorted_end_times = sort_times(new_log)
-    print(f"sorted_start_times\n{sorted_start_times}")
-    print(f"sorted_end_times\n{sorted_end_times}")
     
-    unique_start_times, unique_end_times = get_unique_times(sorted_start_times, 0), get_unique_times(sorted_end_times, 1)
-    print(f"unique_start_times\n{unique_start_times}")
-    print(f"unique_end_times\n{unique_end_times}")
+    unique_start_times = get_unique_times(sorted_start_times, 0)
+    unique_end_times = get_unique_times(sorted_end_times, 1)
     
     merged_list = merge(unique_start_times, unique_end_times)
-    print(f"merged_list\n{merged_list}")
     
     combinations = create_combinations(merged_list)
-    print(f"combinations\n{combinations}")
     
-    divisions = []
+    list_players = {player: 0.0 for player in log}
+    
     for combination in combinations:
-        tmp = []
-        for k, v in new_log.items():
-            if is_within_interval(combination, v):
-                tmp.append(k)
-        divisions.append({'interval': combination, 'player(s)': tmp})
-    print(f"divisions\n{divisions}")
+        active_players = [player for player, interval in new_log.items() if is_within_interval(combination, interval)]
+        if active_players:
+            elapsed_time = calculate_elapsed_time(combination[0], combination[1])
+            split_time = elapsed_time / len(active_players)
+            
+            for player in active_players:
+                list_players[player] += split_time
     
-    new_divisions = []
-    for division in divisions:
-        elapsed_time = calculate_elapsed_time(division['interval'][0], division['interval'][1])
-        new_divisions.append({elapsed_time: division['player(s)']})
-    print(f"new_divisions\n{new_divisions}")
-    
-    for player, fees in list_players.items():
-        print(f"player {player} fees {fees}")
-        for division in new_divisions:
-            print(f"division {division}")
-            for k, v in division.items():
-                print(f"k {k} v {v}")
-                if player in v:
-                    list_players[player] += (k / len(v))
-    print(list_players)
-    
-    for player, fees in list_players.items():
-        list_players[player] = fees * cost_per_hour
+    for player in list_players:
+        list_players[player] *= cost_per_hour
     
     print(list_players)
 
